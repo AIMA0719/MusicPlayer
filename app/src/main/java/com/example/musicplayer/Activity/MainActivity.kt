1ef3bc16d5702d7c0ab6bdacd869a26218655b3d
Infocar <dkwkrhrh07@naver.com>
2024-12-31 16:52:39 +0900

    12/31 코드 정리 및 주석 추가

--- app/src/main/java/com/example/musicplayer/Activity/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/Activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicplayer.Activity
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.musicplayer.Fragment.MusicListFragment
@@ -11,26 +12,47 @@ import com.example.musicplayer.databinding.MusicPlayerMainActivityBinding
 import com.example.musicplayer.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: MusicPlayerMainActivityBinding
+    private lateinit var binding: MusicPlayerMainActivityBinding
     private lateinit var toastManager: ToastManager
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MusicPlayerMainActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = MusicPlayerMainActivityBinding.inflate(layoutInflater) // 객체 바인딩
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java] // 메인 뷰 모델
 
-        setBaseSetting()
-        hideActionBar()
-        observeViewModel()
-        PermissionManager(this).checkPermission()
+        setContentView(binding.root) // 화면 세팅
+        setBaseSetting() // 메인 context 및 메인 activity 캐싱
+        hideActionBar() // 액션 바 숨기기
+        observeViewModel() // 화면 이동 및 토스트 메시지 처리
+        PermissionManager(this).checkPermission() // 앱 실행 시 권한 요청
+        setOnBackPressed() // 페이지 별 뒤로 가기 처리
+        setUserInterFace() // 버튼 클릭 리스너 세팅
+    }
 
-        // Fragment 변경 이벤트 처리
+    private fun setUserInterFace() {
+        // todo 일단 화면 클릭 하면 뮤직 리스트로 이동
         binding.flMainLayout.setOnClickListener {
             viewModel.addFragment(MusicListFragment.newInstance(1))
         }
+    }
+
+    private fun setOnBackPressed() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (viewModel.isFragmentStackEmpty()) {
+                    if (viewModel.isDoubleBackToExit()) {
+                        toastManager.removeAnimationToast()
+                        finish()
+                    } else {
+                        viewModel.triggerDoubleBackToExit()
+                    }
+                } else {
+                    viewModel.popFragment()
+                }
+            }
+        })
     }
 
     private fun setBaseSetting() {
@@ -52,20 +74,6 @@ class MainActivity : AppCompatActivity() {
         // Toast 메시지 처리
         viewModel.toastMessage.observe(this) { message ->
             toastManager.showAnimatedToast(message)
-        }
-    }
-
-    override fun onBackPressed() {
-        if (viewModel.isFragmentStackEmpty()) {
-            if (viewModel.isDoubleBackToExit()) {
-                super.onBackPressed()
-                toastManager.removeAnimationToast()
-                finish()
-                return
-            }
-            viewModel.triggerDoubleBackToExit()
-        } else {
-            viewModel.popFragment()
         }
     }
 }
